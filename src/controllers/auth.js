4d5563bd1a592fe2d2bf1a041f04b45591a7f98f
Linkon Siddique <linkon.sq48@gmail.com>
2024-11-01 00:33:26 +0600

    Hide private data

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -57,7 +57,7 @@ exports.login = async (req, res, next) => {
     res.status(200).json({
       message: "Login successful",
       token: token,
-      user: user,
+      user,
     });
   } catch (err) {
     if (!err.statusCode) {
