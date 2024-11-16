42b775991a51d3fb8dd6ab29860ca2bd2755a098
Linkon Siddique <linkon.sq48@gmail.com>
2024-11-16 10:26:20 +0600

    Validate file uploads

--- src/middleware/file-upload.js
+++ src/middleware/file-upload.js
@@ -2,4 +2,14 @@ const multer = require("multer");
 
 module.exports = multer({
   dest: "avatars",
+  limits: {
+    fileSize: 1000000, // 1MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload an image"));
+    }
+
+    cb(undefined, true); // accept the file
+  },
 });
